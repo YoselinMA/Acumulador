86bb86701b772374673dd213b8334258c5291a1a
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-05-08 20:42:06 -0600

    Create Acumulador.java

---
+++ Acumulador.java
@@ -0,0 +1,11 @@
+public class Acumulador {
+    public static void main(String[] args) {
+        int una_variable;
+        una_variable = 3;
+        una_variable = una_variable + 5;
+        una_variable ++;
+        System.out.println( una_variable);
+    
+    }
+    
+}
